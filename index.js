f32dbe6167bd68e4adb0dd3db3971c18671ddb09
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-04-07 16:31:39 -0700

    Completes lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2; 
+
+const random = Math.floor(Math.random()*1000)+1; 
+
+const num3 = 22;
+const num4 = 6;
+
+const mod = num3 % num4; 
+
+const max = Math.max(3, 7, 9, 15, 20);
